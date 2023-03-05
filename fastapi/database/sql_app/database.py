2a75145045d7508491c178c2fe1d20ae7d712b7b
masatoEmata <jng.bosn@gmail.com>
2023-03-05 23:48:45 +0900

    addModelsTest

--- fastapi/database/sql_app/database.py
+++ fastapi/database/sql_app/database.py
@@ -1,15 +1,32 @@
+'''
+import the SQLAlchemy parts
+'''
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+'''
+Create a database URL for SQLAlchemy
+'''
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
+
+'''
+Create the SQLAlchemy engine
+'''
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
 )
 # connect_args is needed only for SQLite
 
+'''
+Create a SessionLocal class
+'''
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+'''
+Create a Base class
+'''
 Base = declarative_base()
 
